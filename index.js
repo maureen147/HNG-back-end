79401ee725b1f537bf29c0f46892595bd5242d52
maureen <bisiattah@yahoo.com>
2024-07-03 02:44:11 +0100

    updated

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 import express from 'express';
-import requestIp from "request-ip";
+import axios from "axios";
 
 
 // const express = require('express');
 // const requestIp = require('request-ip');
 const app = express();
-
 const PORT = 6000;
 
-app.get('/', (req, res) => {
-    const clientIP = req.clientIp;
+app.get('/api/hello', (req, res) => {
+   
+    const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
     const clientLocation = "New York";
     const temperature = 11;
     const greeting = `Hello, Mark!, the temperature is ${temperature} degrees Celsius in ${clientLocation}`;
 
+    console.log(`clientIp: ${clientIp},ClientLocation: ${clientLocation},temperature:${temperature},greeting:${greeting}`);
+
     const responseData = {
-        client_ip: clientIP,
+        clientIp: clientIp,
         location: clientLocation,
+        temperature: temperature,
         greeting: greeting
     };
 
@@ -30,3 +33,4 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     
 });
+
